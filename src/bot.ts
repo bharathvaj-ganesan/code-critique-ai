9c135da37ad869d497764606c4cd0decdbcbf95b
Bharathvaj Ganesan <bharathvaj1995@gmail.com>
2024-04-15 16:59:10 +0530

    fix: credentials

--- src/bot.ts
+++ src/bot.ts
@@ -24,7 +24,13 @@ export class Bot {
   constructor(options: Options, bedrockOptions: BedrockOptions) {
     this.options = options
     this.bedrockOptions = bedrockOptions
-    this.client = new BedrockRuntimeClient({})
+    this.client = new BedrockRuntimeClient({
+      region: 'us-east-1',
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? ''
+      }
+    })
   }
 
   chat = async (message: string, prefix?: string): Promise<[string, Ids]> => {
